f6fdcdf5abe4333e93e0900c86de332e2afd2fad
bdfkjadadkjasbkjasdbkjdasjkdas <dmitrenko.2009@list.ru>
2025-06-03 20:26:38 +0700

    Update form1.cs

--- form1.cs
+++ form1.cs
@@ -1,9 +1,9 @@
 using Microsoft.VisualBasic.ApplicationServices;
 using System.Text;
 using System.Text.Json;
-using WinFormsApp5;
+using System.Security.Cryptography;
 
-namespace WinFormsApp5
+namespace WinFormsApp11
 {
     public partial class Form1 : Form
     {
@@ -12,6 +12,21 @@ namespace WinFormsApp5
             InitializeComponent();
             cmbRole.Items.AddRange(new string[] { "admin", "worker", "client" });
             cmbRole.SelectedIndex = 0;
+        }
+
+        private string GetHashSHA256(string plainText)
+        {
+            string hashText = "";
+            Encoding enc = Encoding.UTF8;
+            using (SHA256Managed hash = new SHA256Managed())
+            {
+                byte[] result = hash.ComputeHash(enc.GetBytes(plainText));
+                foreach (byte item in result)
+                {
+                    hashText += item.ToString("X2");
+                }
+            }
+            return hashText;
         }
 
         private void ClearRegistrationFields()
@@ -41,30 +56,21 @@ namespace WinFormsApp5
             }
             return false;
         }
+
         private void RegisterUser(string username, string password, string role)
         {
             string filePath = "users.txt";
-            string userRecord = $"{username},{password},{role}";
+            string hashedPassword = GetHashSHA256(password);
+            string userRecord = $"{username},{hashedPassword},{role}";
 
             File.AppendAllText(filePath, userRecord + Environment.NewLine);
         }
 
-        private void btnRegister_Click_1(object sender, EventArgs e)
+        private void btnRegister_Click(object sender, EventArgs e)
         {
             string username = txtUsername.Text;
             string password = txtPassword.Text;
             string role = cmbRole.SelectedItem?.ToString();
-
-
-            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password) || string.IsNullOrEmpty(role))
-            {
-                MessageBox.Show("Пожалуйста, заполните все поля",
-                                "Ошибка ввода",
-                                MessageBoxButtons.OK,
-                                MessageBoxIcon.Warning);
-                return;
-            }
-
 
             if (password.Length < 6)
             {
@@ -75,10 +81,9 @@ namespace WinFormsApp5
                 return;
             }
 
-
             if (role != "admin" && role != "client" && role != "worker")
             {
-                MessageBox.Show("Роль может быть только 'admin',  'client' или 'worker'",
+                MessageBox.Show("Роль может быть только 'admin', 'client' или 'worker'",
                                 "Недопустимая роль",
                                 MessageBoxButtons.OK,
                                 MessageBoxIcon.Error);
@@ -117,7 +122,7 @@ namespace WinFormsApp5
             }
         }
 
-        private void btnLogins_Click(object sender, EventArgs e)
+        private void btnLogin_Click(object sender, EventArgs e)
         {
             Form2 Forms = new Form2();
             Forms.Show();
